f201f877dd1f0d5b9ee524a3d50b1c728681fd05
yasmineejama <yasmineejama@gmail.com>
2025-08-08 21:03:30 +0200

    complete

--- src/script.js
+++ src/script.js
@@ -26,20 +26,25 @@ const contactBook = {
 };
   
   // Ange en grupp
-  let groupToShow;
+  let groupToShow = "vänner";
   
 
 
 
-function showContacts(contactBook, groupToShow) { // Rör ej denna kod
-
-
+function showContacts(contactBook, groupToShow) { 
+  // Rör ej denna kod
 
     // Loopar igenom alla kontakter och jämför grupp
-   
-    return  // här ska sträng med namn och tekefonnummer visas i form av string interpolation
 
+    for (contact of contactBook.contacts) {
 
+      if (contact.group === groupToShow) {
+
+        return `Namn: ${contact.name}, Telefon: ${contact.phone}`;  
+        // här ska sträng med namn och tekefonnummer visas i form av string interpolation
+
+      }
+    }
 }
 
 
